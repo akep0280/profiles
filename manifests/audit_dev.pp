fa70317c665f84ac723b2c6ba12e65e6c260e5b9
Jerald Sheets <questy@gmail.com>
2014-12-03 21:34:27 -0800

    removing redundant profiles

--- manifests/audit_dev.pp
+++
@@ -1,8 +0,0 @@
-# Not Audited
-class profiles::audit_dev {
-  include profiles::base
-
-  package { 'audit':
-    ensure => absent,
-  }
-}
--- manifests/audit_prod.pp
+++ manifests/audit_prod.pp
@@ -1,5 +1,8 @@
 # Audited
 class profiles::audit_prod {
+  # Hiera Lookups
+  $installed = hiera('audit_installed')
+
   include profiles::base
   include audit
 }
