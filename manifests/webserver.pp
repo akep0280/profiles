72da10066a75cc1535efd5010ba4ca480f12b8e6
Jerald Sheets <questy@gmail.com>
2015-06-03 00:22:48 -0400

    using vcsrepo to keep it up to date

--- manifests/webserver.pp
+++ manifests/webserver.pp
@@ -12,6 +12,7 @@ class profiles::webserver (
     www_root    => '/var/www/www.pltest.com',
     listen_port => "${port}",
     require     => File['/var/www/www.pltest.com'],
+    notify      => Vcsrepo['/var/www/www.pltest.com'],
   }
 
   # Make sure the parent webroot exists
@@ -30,17 +31,18 @@ class profiles::webserver (
     mode   => '0755',
   }
 
-  # Clone the sample data to our docroot
+  # Clone the sample data to our docroot only the first time
   exec {'getit':
     path    => '/bin:/usr/bin',
     command => '/usr/bin/git clone https://github.com/puppetlabs/exercise-webpage.git /var/www/www.pltest.com',
-    require => [ File['/var/www/www.pltest.com'], Exec['cleanit'] ],
+    unless  => '/usr/bin/test -f /var/www/www.pltest.com/index.html',
+    require => File['/var/www/www.pltest.com'],
   }
 
-  # Clear the old content before pulling the latest
-  exec {'cleanit':
-    path    => '/bin:/usr/bin',
-    command => '/bin/rm -rf /var/www/www.pltest.com',
-    onlyif  => '/usr/bin/test -f /var/www/www.pltest.com',
+  # Refresh the data on every run
+  vcsrepo {'/var/www/www.pltest.com':
+    ensure   => 'latest',
+    provider => 'git',
+    source   => 'https://github.com/puppetlabs/exercise-webpage.git',
   }
 }
