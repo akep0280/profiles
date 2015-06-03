de79b72c4352881e57121912e315e3e3dc99aec3
Jerald Sheets <questy@gmail.com>
2015-06-03 00:28:22 -0400

    removing redundant code

--- manifests/webserver.pp
+++ manifests/webserver.pp
@@ -31,18 +31,12 @@ class profiles::webserver (
     mode   => '0755',
   }
 
-  # Clone the sample data to our docroot only the first time
-  exec {'getit':
-    path    => '/bin:/usr/bin',
-    command => '/usr/bin/git clone https://github.com/puppetlabs/exercise-webpage.git /var/www/www.pltest.com',
-    unless  => '/usr/bin/test -f /var/www/www.pltest.com/index.html',
-    require => File['/var/www/www.pltest.com'],
-  }
 
   # Refresh the data on every run
   vcsrepo {'/var/www/www.pltest.com':
     ensure   => 'latest',
     provider => 'git',
     source   => 'https://github.com/puppetlabs/exercise-webpage.git',
+    require  => File['/var/www/www.pltest.com'],
   }
 }
